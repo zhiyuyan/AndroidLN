126a19db9069fc9818fbf1833cb11161cc9b3bec
颜志煜 <152831@nd.com>
2018-12-19 19:37:45 +0800

    TabItem使用

---
+++ app/src/main/java/com/example/yzy/androidln/tablayout/TabItemActivity.java
@@ -0,0 +1,15 @@
+package com.example.yzy.androidln.tablayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yzy.androidln.R;
+
+public class TabItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_item);
+    }
+}
