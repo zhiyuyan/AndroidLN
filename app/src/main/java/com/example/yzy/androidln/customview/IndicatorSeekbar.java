997d45bc552b6e58ecaeef29e4ad6c1892ff682a
颜志煜 <152831@nd.com>
2019-02-02 19:28:57 +0800

    IndicatorSeekbar

--- app/src/main/java/com/example/yzy/androidln/customview/IndicatorSeekbar.java
+++ app/src/main/java/com/example/yzy/androidln/customview/IndicatorSeekbar.java
@@ -5,9 +5,11 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -36,6 +38,17 @@ public class IndicatorSeekbar extends View {
     private float mProgress; // real time value
     private float mMin; // min
     private float mMax; // max
+    private float mDelta; // max - min
+    private float mDeltaX;
+
+    // 进度文字位置信息
+    private Rect mProgressTextRect = new Rect();
+
+    // 是否是拖动thumb
+    private boolean mIsThumbOnDragging;
+
+    // 是否点击track，直接跳转到某数值
+    private boolean mIsTouchToSeek;
 
     public IndicatorSeekbar(Context context) {
         this(context, null);
@@ -61,6 +74,7 @@ public class IndicatorSeekbar extends View {
         mTrackSize = 120;
         mMin = 0;
         mMax = 100;
+        mDelta = mMax - mMin;
         mProgress = 40;
     }
 
@@ -76,8 +90,85 @@ public class IndicatorSeekbar extends View {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                performClick();
+                getParent().requestDisallowInterceptTouchEvent(true);
+
+                mIsThumbOnDragging = isThumbTouched(event);
+                if (mIsThumbOnDragging) {
+                    invalidate();
+                } else if (isTrackTouched(event)) {
+                    mIsThumbOnDragging = true;
+
+                    mThumbCenterX = event.getX();
+                    if (mThumbCenterX < mLeft) {
+                        mThumbCenterX = mLeft;
+                    }
+                    if (mThumbCenterX > mRight) {
+                        mThumbCenterX = mRight;
+                    }
+                    mProgress = calculateProgress();
+                    invalidate();
+                }
+                mDeltaX = mThumbCenterX - event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mIsThumbOnDragging) {
+                    mThumbCenterX = event.getX() + mDeltaX;
+                    if (mThumbCenterX < mLeft) {
+                        mThumbCenterX = mLeft;
+                    }
+                    if (mThumbCenterX > mRight) {
+                        mThumbCenterX = mRight;
+                    }
+                    mProgress = calculateProgress();
+                    invalidate();
+                }
+                break;
+        }
+        return mIsThumbOnDragging || mIsTouchToSeek || super.onTouchEvent(event);
+    }
+
+    /**
+     * 判断是否是触摸Thumb
+     *
+     * @param event 触摸事件
+     * @return
+     */
+    private boolean isThumbTouched(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+        int xLeft = getPaddingLeft() + mThumbWidth / 2;
+        float distance = xLeft + mTrackLength / mDelta * (mProgress - mMin);
+        if (event.getX() >= distance - mThumbWidth / 2
+                && event.getX() <= distance + mThumbWidth / 2) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否是触摸track
+     *
+     * @param event 触摸事件
+     * @return
+     */
+    private boolean isTrackTouched(MotionEvent event) {
+        return isEnabled() && event.getX() >= getPaddingLeft() && event.getX() <= getMeasuredWidth() - getPaddingRight()
+                && event.getY() >= getPaddingTop() && event.getY() <= getMeasuredHeight() - getPaddingBottom();
+    }
+
+    private float calculateProgress() {
+        return (mThumbCenterX - mLeft) * mDelta / mTrackLength + mMin;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
 
+        // track的左侧起始点
         int xLeft = getPaddingLeft() + mThumbWidth / 2;
         int xRight = getMeasuredWidth() - getPaddingRight() - mThumbWidth / 2;
         float yTop = getPaddingTop() + mThumbHeight / 2;
@@ -85,6 +176,7 @@ public class IndicatorSeekbar extends View {
         mThumbCenterX = xLeft + mTrackLength * (mProgress - mMin) / (mMax - mMin);
 
         // draw track
+        mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(mTrackSize);
         canvas.drawLine(xLeft, yTop, mThumbCenterX, yTop, mPaint);
@@ -94,10 +186,34 @@ public class IndicatorSeekbar extends View {
         mPaint.setStrokeWidth(mTrackSize);
         canvas.drawLine(mThumbCenterX, yTop, xRight, yTop, mPaint);
 
+        mPaint.setTextSize(sp2px(14));
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        // thumb偏移量
+        float thumbX = mThumbCenterX;
+        float thumbY = getHeight() / 2f + mProgressTextRect.height() / 2f;
+
+        // draw min text
+        canvas.drawText(String.valueOf(mMin), mLeft + 48, thumbY, mPaint);
+
+        // draw max text
+        canvas.drawText(String.valueOf(mMax), mRight - 48, thumbY, mPaint);
 
         // draw thumb
-        mPaint.setColor(Color.BLUE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawRoundRect(mThumbCenterX - mThumbWidth / 2, yTop - mThumbHeight / 2 + 2, mThumbCenterX + mThumbWidth / 2, yTop + mThumbHeight / 2 - 2, 20, 20, mPaint);
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRoundRect(mThumbCenterX - mThumbWidth / 2, yTop - mThumbHeight / 2, mThumbCenterX + mThumbWidth / 2, yTop + mThumbHeight / 2, 20, 20, mPaint);
+
+        String progressText = String.valueOf(Math.round(mProgress));
+        mPaint.setTextSize(sp2px(14));
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.getTextBounds(progressText, 0, progressText.length(), mProgressTextRect);
+        canvas.drawText(progressText, thumbX, thumbY, mPaint);
     }
 
     static int dp2px(int dp) {
