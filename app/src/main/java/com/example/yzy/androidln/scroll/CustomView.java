fdf751fc0f30345860da4b9882814748235dddd6
颜志煜 <152831@nd.com>
2019-02-17 19:19:52 +0800

    IndicatorSeekbar

--- app/src/main/java/com/example/yzy/androidln/scroll/CustomView.java
+++ app/src/main/java/com/example/yzy/androidln/scroll/CustomView.java
@@ -5,22 +5,43 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
 
 public class CustomView extends View {
 
     private int mLastX;
     private int mLastY;
 
+    private Scroller mScroller;
+
     public CustomView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mScroller = new Scroller(context);
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            ((View)getParent()).scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    public void smoothScrollTo(int destX, int destY) {
+        int scrollX = getScrollX();
+        int delta = destX - scrollX;
+        mScroller.startScroll(scrollX, 0, delta, 0, 4000);
+        invalidate();
     }
 
     @Override
@@ -29,7 +50,7 @@ public class CustomView extends View {
         int x = (int) event.getX();
         int y = (int) event.getY();
 
-        switch (event.getAction()) {
+        switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
                 mLastX = x;
                 mLastY = y;
@@ -38,9 +59,26 @@ public class CustomView extends View {
                 // 计算移动的距离
                 int offsetX = x - mLastX;
                 int offsetY = y - mLastY;
-                // 调用layout方法来重新放置它的位置
-                layout(getLeft() + offsetX, getTop() + offsetY,
-                        getRight() + offsetX, getBottom() + offsetY);
+
+                // 方法一：layout()方法
+//                // 调用layout方法来重新放置它的位置
+//                layout(getLeft() + offsetX, getTop() + offsetY,
+//                        getRight() + offsetX, getBottom() + offsetY);
+
+                // 方法二：offsetLeftAndRight()和offsetTopAndBottom()方法
+//                // 对left和right进行偏移
+//                offsetLeftAndRight(offsetX);
+//                // 对top和bottom进行偏移
+//                offsetTopAndBottom(offsetY);
+
+                // 方法三：改变布局参数LayoutParams
+//                ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) getLayoutParams();
+//                layoutParams.leftMargin = getLeft() + offsetX;
+//                layoutParams.topMargin = getTop() + offsetY;
+//                setLayoutParams(layoutParams);
+
+                // 方法四：scrollTo与scrollBy
+                ((View)getParent()).scrollBy(-offsetX, -offsetY);
                 break;
         }
         return true;
--- app/src/main/java/com/example/yzy/androidln/scroll/SlideDemoActivity.java
+++ app/src/main/java/com/example/yzy/androidln/scroll/SlideDemoActivity.java
@@ -1,9 +1,20 @@
 package com.example.yzy.androidln.scroll;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.yzy.androidln.R;
 
 public class SlideDemoActivity extends AppCompatActivity {
 
+    private CustomView mCustomView;
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_demo);
+        mCustomView = findViewById(R.id.custom_view);
+        mCustomView.smoothScrollTo(-800, 0);
+    }
 }
