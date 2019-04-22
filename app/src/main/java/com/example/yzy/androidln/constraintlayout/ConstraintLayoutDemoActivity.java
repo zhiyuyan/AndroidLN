82b51f298de486f7e796da5ce4069e8345e73f48
颜志煜 <152831@nd.com>
2019-04-22 14:35:24 +0800

    ConstraintLayout

---
+++ app/src/main/java/com/example/yzy/androidln/constraintlayout/ConstraintLayoutDemoActivity.java
@@ -0,0 +1,15 @@
+package com.example.yzy.androidln.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yzy.androidln.R;
+
+public class ConstraintLayoutDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_demo);
+    }
+}
